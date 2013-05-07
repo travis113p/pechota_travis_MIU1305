673e67cae5697dc004c06f7e19371789f3225c2a
travis113p <travis113p@fullsail.edu>
2013-05-06 23:03:59 -0400

    inital commit.  files from VFW

---
+++ js/json.js
@@ -0,0 +1,36 @@
+/*
+Travis Pechota
+VFW 1304 - Project 4
+json.js
+*/
+
+var json = {
+	"pizza1": {
+		"pizzasize": ["Pizza Size:", "Small"],
+		"pizzastyle": ["Pizza Style:", "Deep Dish"],
+		"pepperoni": ["Pepperoni:", "No"],
+		"sausage": ["Sausage:", "Yes"],
+		"ham": ["Ham:", "No"],
+		"gp": ["Green Peppers:", "No"],
+		"bp": ["Banana Peppers:", "Yes"],
+		"bacon": ["Bacon:", "Yes"],
+		"fc": ["Feta Cheese", "No"],
+		"sauce": ["Sauce:", "7"],
+		"date": ["Delivery Date:", "2013-05-22"],
+		"comments": ["Additional Comments:", "Cut in quarters."]
+	},
+	"pizza2": {
+		"pizzasize": ["Pizza Size:", "Medium"],
+		"pizzastyle": ["Pizza Style:", "Thin Crust"],
+		"pepperoni": ["Pepperoni:", "Yes"],
+		"sausage": ["Sausage:", "Yes"],
+		"ham": ["Ham:", "Yes"],
+		"gp": ["Green Peppers:", "Yes"],
+		"bp": ["Banana Peppers:", "Yes"],
+		"bacon": ["Bacon:", "No"],
+		"fc": ["Feta Cheese", "Yes"],
+		"sauce": ["Sauce:", "4"],
+		"date": ["Delivery Date:", "2013-05-29"],
+		"comments": ["Additional Comments:", "Allergic to dogs."]
+	}
+};
